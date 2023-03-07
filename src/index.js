aeacb4172d4910e0b9e20ffca2b3bed63660f48a
joejames0830 <kyeun950830@gmail.com>
2023-03-07 20:33:39 +0900

    Expense items

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+// 메인 엔트리/ 메인 훅 생성
+root.render(<App />);
